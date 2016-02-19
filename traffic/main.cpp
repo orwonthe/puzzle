5348dae3b5450ef46a3c048e2b80e9565bcdba85
Willy Mills <orwonthe@gmail.com>
2016-02-18 19:06:44 -0700

    Added all of the puzzles.

--- traffic/main.cpp
+++ traffic/main.cpp
@@ -6,8 +6,7 @@ void solve_and_explain(int puzzle_number) {
 }
 
 int main() {
-    int puzzles[] = {-1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 37, 38, 39, 40};
-    for (int puzzle_number : puzzles) {
+    for (int puzzle_number=1; puzzle_number<=40; puzzle_number++) {
         solve_and_explain(puzzle_number);
     }
     return 0;
